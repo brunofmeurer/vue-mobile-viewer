b7c62676bdc9459e122a23a2b5f9e358c43141a0
Bruno Meurer <brunofmeurer@gmail.com>
2018-09-03 20:38:28 -0300

    push all

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-push-dir',
+    assetsPublicPath: '/vue-mobile-viewer',
 
     /**
      * Source Maps
