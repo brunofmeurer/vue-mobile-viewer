18d63cd64f8a2022b4c71c0c30c7600de87978a1
Bruno Meurer <brunofmeurer@gmail.com>
2018-09-03 21:20:49 -0300

    all

---
+++ src/page/Device.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <viewer :device="picked" :url="url"/>
+  </div>
+</template>
+<script>
+import Viewer from '@/components/viewer/Viewer'
+export default {
+  components: {
+    Viewer
+  },
+  data () {
+    return {
+      picked: 'android',
+      url: 'https://union.co/'
+    }
+  },
+  created () {
+    this.url = this.$route.query.url
+    this.picked = this.$route.query.type
+  }
+}
+</script>
+
+<style>
+.field input{
+  margin: 20px 0px;
+  width: 500px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Init from '@/page/Init'
+import Device from '@/page/Device'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'init',
       component: Init
+    },
+    {
+      path: '/device',
+      name: 'Device',
+      component: Device
     }
   ]
 })
